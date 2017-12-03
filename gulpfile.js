bddf8240bfd86d6b55404afe3de7062469074037
Vladimir <prosto24061989@gmail.com>
2017-12-03 15:15:39 +0200

    add smart-grid

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ var gulp           = require('gulp'),
 		ftp            = require('vinyl-ftp'),
 		notify         = require("gulp-notify"),
 		rsync          = require('gulp-rsync');
+		smartgrid      = require('smart-grid');
 
 // Пользовательские скрипты проекта
 
@@ -122,6 +123,44 @@ gulp.task('rsync', function() {
 		compress: true
 	}));
 });
+
+/* It's principal settings in smart grid project */
+var settings = {
+    outputStyle: 'sass', /* less || scss || sass || styl */
+    columns: 12, /* number of grid columns */
+    offset: '30px', /* gutter width px || % */
+    mobileFirst: false, /* mobileFirst ? 'min-width' : 'max-width' */
+    container: {
+        maxWidth: '1200px', /* max-width оn very large screen */
+        fields: '30px' /* side fields */
+    },
+    breakPoints: {
+        lg: {
+            width: '1100px', /* -> @media (max-width: 1100px) */
+        },
+        md: {
+            width: '960px'
+        },
+        sm: {
+            width: '780px',
+            fields: '15px' /* set fields only if you want to change container.fields */
+        },
+        xs: {
+            width: '560px'
+        }
+        /* 
+        We can create any quantity of break points.
+ 
+        some_name: {
+            width: 'Npx',
+            fields: 'N(px|%|rem)',
+            offset: 'N(px|%|rem)'
+        }
+        */
+    }
+};
+ 
+smartgrid('./app/sass/', settings);
 
 gulp.task('removedist', function() { return del.sync('dist'); });
 gulp.task('clearcache', function () { return cache.clearAll(); });
